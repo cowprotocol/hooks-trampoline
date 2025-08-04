8a3b9821afe8782dbf970bd325c5e627b806f71c
Anxo Rodriguez <anxo@cow.fi>
2025-08-04 09:11:47 +0100

    Use absolute path
    Co-authored-by: Federico Giacon <58218759+fedgiac@users.noreply.github.com>

--- test/GasLimitEnforcement.t.sol
+++ test/GasLimitEnforcement.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8;
 
 import {Test} from "forge-std/Test.sol";
 
-import {HooksTrampoline} from "../src/HooksTrampoline.sol";
+import {HooksTrampoline} from "src/HooksTrampoline.sol";
 
 contract GasLimitEnforcementTest is Test {
     address public settlement;
